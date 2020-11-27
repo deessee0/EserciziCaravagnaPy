121feb4e0abf7d281240332ddd876ceeeb36ecfe
Alberto Di <alberto.disalvia@gmail.com>
2020-11-27 11:23:10 +0000

    Inizio es2

--- main.py
+++ main.py
@@ -3,6 +3,27 @@ class Viaggio:
     def __init__(self, nome, dataP, dataR, localita, resort, prezzo, partecipanti, responsabile):
         
         self.nome_viaggio = nome
-        
+        self.dataP = dataP
+        self.dataR = dataR
+        self.localita = localita
+        self.resort = resort
+        self.prezzo = prezzo
+        self.partecipanti = partecipanti 
+        self.responsabile = responsabile 
+
+    def stampa(self):
+        pass
     
+    def periodo(self):
+        pass
+
+    def guadagno(self):
+        pass
+
+class VacanzaInvernale(Viaggio):
+    pass
+
+class VacanzaEstiva(Viaggio):
+    pass
+
 
